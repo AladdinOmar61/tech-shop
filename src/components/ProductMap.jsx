077e858134d57cd9f601bf6b5ca3cbe75427b65d
AladdinOmar61 <Aladdin.Omar61@gmail.com>
2023-02-14 13:56:36 -0500

    added a data file
    moved products from each component into a separate files for each component to import from

--- src/components/ProductMap.jsx
+++ src/components/ProductMap.jsx
@@ -1,12 +1,5 @@
+ import products from "../Data";
  const ProductMap = () => {
-
-    const products = [
-        { id: 1, name: "Apple iPhone", price: 999, onSale: false },
-        { id: 2, name: "Samsung Galaxy S21", price: 799, onSale: true },
-        { id: 3, name: "Google Pixel 5", price: 699, onSale: false },
-        { id: 4, name: "OnePlus 9 Pro", price: 899, onSale: true },
-        { id: 5, name: "AirPods Pro", price: 249, onSale: false },
-      ];
  
  const productItems = products.map(({id, name, price}) => {
     return <div key={id} className="prod">
